e3a134da5dfb3d5d8a33aae11fc0cdb98198ad37
William Kennedy <williamkennedyjnr@gmail.com>
2026-06-09 12:00:49 +0100

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://citizendata.ie',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
